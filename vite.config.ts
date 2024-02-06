1b815bd9f7b68595d7df0475f3b33a1362fd6268
Simon <contact@smn.codes>
2024-02-06 18:12:07 +0100

    Package and types config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
@@ -11,8 +12,12 @@ export default defineConfig({
       formats: [
         'iife',
         'es',
+        'umd',
       ]
 		}
   },
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    dts({ rollupTypes: true })
+  ],
 })
