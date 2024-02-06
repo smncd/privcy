ab87cf2f9277fd708c3e977c6daff5d1f3caa3c0
Simon <contact@smn.codes>
2024-02-06 23:38:18 +0100

    Pages job

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import dts from 'vite-plugin-dts'
 
+const isDocs = process.env.NODE_ENV == 'docs';
+
 export default defineConfig({
-  build: {
+  build:!isDocs &&  {
     lib: {
 			entry: resolve(__dirname, 'src/main.ts'),
 			name: 'Privcy',
@@ -17,6 +19,6 @@ export default defineConfig({
   },
   plugins: [
     svelte(),
-    dts({ rollupTypes: true })
+    !isDocs && dts({ rollupTypes: true })
   ],
 })
