3acea3ba833dba169b20f0273a3246f765753425
Simon <contact@smn.codes>
2024-02-07 02:43:27 +0100

    Iframe fallback style tweak

--- packages/privcy/src/components/IframeFallback.svelte
+++ packages/privcy/src/components/IframeFallback.svelte
@@ -12,8 +12,15 @@
   export let buttonCallback: () => void;
 </script>
 
-<main
+<div
   style="
+    outline: 2px solid currentColor;
+    font-family: monospace;
+    text-align: center;
+  "
+>
+  <main
+    style="
       margin: 0 auto;
       height: 100%;
       width: min(400px, 100%);
@@ -21,25 +28,25 @@
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      font-family: monospace;
-      text-align: center;
+
     "
->
-  <h1>Privacy</h1>
-  <p>
-    Viewing the content in this iframe requires enabling '{categoryName}'
-    in the privacy settings.
-  </p>
-  <button
-    on:click={buttonCallback}
-    style="
+  >
+    <h1>Privacy</h1>
+    <p>
+      Viewing the content in this iframe requires enabling '{categoryName}'
+      in the privacy settings.
+    </p>
+    <button
+      on:click={buttonCallback}
+      style="
         border: initial;
         padding: 10px 15px;
         background-color: black;
         color: white;
         cursor: pointer;
       "
-  >
-    Settings
-  </button>
-</main>
+    >
+      Settings
+    </button>
+  </main>
+</div>
