ccc596e12be193de17615fb8af95d7f402d086f7
Simon <contact@smn.codes>
2025-06-10 13:26:55 +0200

    Preamle and terser minification

--- packages/privcy/vite.config.ts
+++ packages/privcy/vite.config.ts
@@ -1,6 +1,17 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import packageJson from './package.json';
+
+const preamble = `/**
+ * Privcy.
+ * Consent banner script.
+ *
+ * @author ${packageJson.author}
+ * @license ${packageJson.license}
+ * @copyright 2024 - present Simon Lagerlöf
+ * @version ${packageJson.version}
+ */`;
 
 export default defineConfig({
   build: {
@@ -8,6 +19,21 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'Privcy',
       fileName: 'privcy',
+    },
+    minify: 'terser',
+    terserOptions:{
+      format: {
+        comments: false,
+        preamble,
+      },
+      compress: {
+        booleans_as_integers: true,
+        keep_fargs: false,
+      },
+      mangle: {
+        keep_classnames: false,
+        reserved: [],
+      },
     },
     rollupOptions: {
       output: [
@@ -20,8 +46,9 @@ export default defineConfig({
           format: 'es',
           entryFileNames: 'privcy.mjs',
         },
-      ]
-    }
+      ],
+
+    },
   },
   plugins: [dts({ rollupTypes: true })],
 });
