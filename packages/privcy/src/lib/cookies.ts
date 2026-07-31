d99ca7ee1398cf834666749c10ffe03a0352c47f
Simon <contact@smn.codes>
2026-07-31 18:56:20 +0200

    fix: uri-encode cookie value

--- packages/privcy/src/lib/cookies.ts
+++ packages/privcy/src/lib/cookies.ts
@@ -2,18 +2,20 @@
  * Get cookie.
  */
 export function getCookie(name: string): string | undefined {
-  return document.cookie
+  const value = document.cookie
     .split(';')
     .find((cookie) => cookie.trim().startsWith(name + '='))
     ?.split('=')
     .pop();
+
+  if (value) return decodeURIComponent(value);
 }
 
 /**
  * Set cookie with preconfigured settings.
  */
 export function setCookie(name: string, value: string): string {
-  document.cookie = `${name}=${value}; expires=${new Date(Date.now() + 180 * 24 * 60 * 60 * 1000).toUTCString()}; SameSite=strict; Secure; path=/;`;
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${new Date(Date.now() + 180 * 24 * 60 * 60 * 1000).toUTCString()}; SameSite=strict; Secure; path=/;`;
 
   return value;
 }
